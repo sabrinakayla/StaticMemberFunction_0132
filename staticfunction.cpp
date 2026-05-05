9747dec442bdbf8f38f51d963b1fc436e9cbdc7d
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-05 18:33:41 +0700

    menambahkan class mahasiswa

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa{
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim){
+        nim = pnim;
+    };
