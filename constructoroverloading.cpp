6d5e199ac6a83a7cb6bedab4bfee75a179cbada2
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-05 18:48:04 +0700

    implementasi constructor overloading untuk fleksibilitas input data

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,24 @@ class Mahasiswa {
         Mahasiswa(int nim, string nama);
         void cetak();
 };
+
+Mahasiswa::Mahasiswa() {}
+
+Mahasiswa::Mahasiswa(int inim) {
+    nim = inim;
+}
+
+
+Mahasiswa::Mahasiswa(string inama) {
+    nama = inama;
+}
+
+Mahasiswa::Mahasiswa(int inim, string inama) {
+    nim = inim;
+    nama = inama;
+}
+
+void Mahasiswa::cetak() {
+    cout << "NIM    : " << nim << endl;
+    cout << "Nama   : " << nama << endl;
+}
