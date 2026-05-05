b738afc53b10995f56ed5d45e4e248b361c19f1a
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-05 19:13:02 +0700

    program chain function call

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+
+public:
+    buku &setJudul(string judul) {
+        this->judul = judul;
+        return *this;   // chain function
+    }
+
+    string getJudul() {
+        return this->judul;
+    }
+};
+
+int main() {
+    buku bukunya;
+
+    // Cara biasa (dikomentari di gambar):
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+
+    // Menggunakan chain function calls:
+    cout << bukunya.setJudul("Matematika").getJudul(); 
+
+    return 0;
+}
