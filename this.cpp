1a73696d058a1c9d9ea70af8bd360f6a6d43c111
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-05 19:08:46 +0700

    program this

---
+++ this.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Buku {
+    string judul;
+    public:
+        string setget(string judul) {
+            this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
+            return this->judul; // return nilai member variabel 'judul'
+        }
+};
+
+int main() 
+{
+    Buku bukunya;
+    cout << bukunya.setget("Matematika");
+    return 0;
+}
