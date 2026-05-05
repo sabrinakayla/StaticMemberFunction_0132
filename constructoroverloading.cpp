67921f96f49a352478557611f3386a12164c00f1
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-05 18:46:38 +0700

    menambahkan class mahasiswa

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Mahasiswa {
+    private:
+        int nim;
+        string nama;
+    public:
+        Mahasiswa();
+        Mahasiswa(int);
+        Mahasiswa(string);
+        Mahasiswa(int nim, string nama);
+        void cetak();
+};
